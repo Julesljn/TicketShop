c5c69dab643ce0ef45844110ed636cfd5cfe875a
Julesljn <juleslejeunepro@gmail.com>
2024-09-25 12:47:16 +0200

    feat: user database/model

--- project/app/Models/User.php
+++ project/app/Models/User.php
@@ -17,9 +17,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
         'email',
         'password',
+        'birthday',
+        'artiste_name',
+        'picture',
+        'role',
     ];
 
     /**
