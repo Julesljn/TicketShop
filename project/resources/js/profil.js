c872d165c25172d4a6956e92ddb4700be55c44ac
Julesljn <juleslejeunepro@gmail.com>
2024-09-27 12:06:05 +0200

    feat: profil css and dom

--- project/resources/js/profil.js
+++ project/resources/js/profil.js
@@ -2,10 +2,27 @@ const deleteBtn = document.querySelector('#btn__delete');
 const containerVerif = document.querySelector('#container__verif');
 const backBtn = document.querySelector('#verifBtn__back');
 
+const modifBtn = document.querySelector('#btn__modif');
+const cancelBtn = document.querySelector('#btn__cancel');
+const uptadeBtn = document.querySelector('#btn__uptade');
+
 deleteBtn.addEventListener('click', () => {
-    console.log('fefef')
     containerVerif.style.display = 'flex';
 })
 backBtn.addEventListener('click', () => {
     containerVerif.style.display = 'none';
+})
+
+
+
+modifBtn.addEventListener('click', () => {
+    console.log('frfr')
+    modifBtn.style.display = 'none';
+    cancelBtn.style.display = 'block';
+    uptadeBtn.style.display = 'block';
+})
+cancelBtn.addEventListener('click', () => {
+    cancelBtn.style.display = 'none';
+    uptadeBtn.style.display = 'none';
+    modifBtn.style.display = 'block';
 })
--- project/resources/views/profil.blade.php
+++ project/resources/views/profil.blade.php
@@ -12,11 +12,58 @@
                     <li><button id="btn__delete">Supprimer mon compte</button></li>
                 </ul>
             </div>
-            <div class="h-full w-[40vw]">
-
+            <div class="h-full w-[40vw] flex">
+                <div class="h-full w-[20vw] mt-10">
+                    <form class="text-white flex flex-col ml-8 gap-6" action="">
+                        <div class="flex flex-col">
+                            <label class="ml-6 mb-2" for="first_name">Prénom</label>
+                            <div class="container__input p-4 border-2 rounded-full w-[18vw]">
+                                <input class="input outline-none bg-transparent placeholder-white w-[25vw]"
+                                    type="text" name="first_name" required value="{{ Auth::user()->first_name }}">
+                            </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="ml-6 mb-2" for="last_name">Nom</label>
+                            <div class="container__input p-4 border-2 rounded-full w-[18vw]">
+                                <input class="input outline-none bg-transparent placeholder-white w-[25vw]"
+                                    type="text" name="last_name" required value="{{ Auth::user()->last_name }}">
+                            </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="ml-6 mb-2" for="email">Email</label>
+                            <div class="container__input p-4 border-2 rounded-full w-[18vw]">
+                                <input class="input outline-none bg-transparent placeholder-white w-[25vw]"
+                                    type="email" name="email" required value="{{ Auth::user()->email }}">
+                            </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="ml-6 mb-2" for="birthday">Date de Naissance</label>
+                            <div class="container__input p-4 border-2 rounded-full w-[18vw]">
+                                <input class="input outline-none bg-transparent placeholder-white w-[25vw]"
+                                    type="date" name="birthday" required value="{{ Auth::user()->birthday }}">
+                            </div>
+                        </div>
+                        <div class="flex flex-col text-white">
+                            <p>Type de compte : <span>"{{ Auth::user()->role }}"</span></p>
+                        </div>
+                        <div class="flex gap-5">
+                            <button id="btn__modif" class="p-3 px-12 border-2 rounded-full text-white text-lg mt-8" type="button">Modifier</button>
+                            <button id="btn__cancel" class="btn__visible p-3 px-12 border-2 border-red-600 rounded-full text-white text-lg mt-8" type="button" style="display: none" >Annuler</button>
+                            <button id="btn__uptade" class="btn__visible p-3 px-12 border-2 rounded-full text-white text-lg mt-8" type="submit" style="display: none" >Valider</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="h-full w-[20vw] flex justify-center mt-10">
+                    <img class="w-60 h-60 rounded-full" src="{{ asset('medias/images/pp.jpg') }}" alt="">
+                </div>
             </div>
         </section>
-        <div id="container__verif" class="absolute w-1/2 h-1/2 bg-black top-[50%] left-[50%] text-white flex flex-col items-center justify-center gap-20" style="display: none">
+
+
+
+        <div id="container__verif"
+            class="absolute w-1/2 h-1/2 bg-black top-[50%] left-[50%] text-white flex flex-col items-center justify-center gap-20"
+            style="display: none">
             <h2 class="text-2xl">Êtes vous sur de vouloir supprimer votre compte ?</h2>
             <div class="flex gap-16">
                 <button id="verifBtn__back" class="py-6 px-10 rounded-full border-2">Retour</button>
